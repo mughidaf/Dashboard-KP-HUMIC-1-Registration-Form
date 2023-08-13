29b8e857a739854e557f73bc801d7205e65104a3
fahrigunawan <fahrigilo991@gmail.com>
2023-08-14 06:18:08 +0700

    Admin Add Form Added
    Tombol yang default emang sengaja gitu karna yang ada style nya ga work

---
+++ public/AdminAddForm.js
@@ -0,0 +1,97 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const addRadio = document.getElementById("radioButton");
+    const addText = document.getElementById("textFieldButton");
+    const formContainerWrapper = document.getElementById("formContainerWrapper");
+
+    //ini buat opsi pertanyaan
+    formContainerWrapper.addEventListener("click", function(event) {
+        if (event.target.classList.contains("Prototype")) {
+            const selectedType = event.target.closest(".form-container, .form-container-1").getAttribute("data-type");
+            const optionContainer = event.target.closest(".form-container, .form-container-1").querySelector(".question-card");
+            const newOption = optionContainer.cloneNode(true);
+
+            optionContainer.parentElement.insertBefore(newOption, optionContainer.nextSibling);
+        }
+    });
+
+    //ini buat button ngapus
+    formContainerWrapper.addEventListener("click", function(event) {
+        if (event.target.classList.contains("remove-button")) {
+            const formContainer = event.target.closest(".form-container, .form-container-1");
+            if (formContainer) {
+                formContainer.remove();
+            }
+        }
+    });
+
+    //ini buat hapus opsi nya
+    formContainerWrapper.addEventListener("click", function(event) {
+        if (event.target.classList.contains("delete-question")) {
+            const formContainer = event.target.closest(".question-card");
+            if (formContainer) {
+                formContainer.remove();
+            }
+        }
+    });
+
+
+
+    //ini buat tiap neken option radio maka akan muncul radio
+    addRadio.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        const selectedType = document.querySelector(".dropdown-content a.active").getAttribute("data-type");
+        const formTemplate = document.querySelector(`.form-template[data-type="${selectedType}"]`);
+        const newForm = formTemplate.cloneNode(true);
+
+        newForm.classList.remove("form-template");
+        //ganti display ssebelumnya dari hidden ke block dan dibikin aktif
+        newForm.style.display = "block";
+        newForm.classList.add("active");
+
+        formContainerWrapper.appendChild(newForm);
+    });
+
+    //ini buat tiap neken option radio maka akan muncul text
+    addText.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        const selectedType = document.querySelector(".dropdown-content a.active").getAttribute("data-type");
+        const formTemplate = document.querySelector(`.form-template[data-type="${selectedType}"]`);
+        const newForm = formTemplate.cloneNode(true);
+
+        newForm.classList.remove("form-template");
+        //ganti display ssebelumnya dari hidden ke block dan dibikin aktif
+        newForm.style.display = "block";
+        newForm.classList.add("active");
+
+        formContainerWrapper.appendChild(newForm);
+    });
+
+    //gabungin radio n text button dan jadiin event listener
+    const radioButtons = document.querySelectorAll("[data-type='radio']");
+    const textFieldButtons = document.querySelectorAll("[data-type='text']");
+
+    //iterate radio button
+    radioButtons.forEach(button => {
+        button.addEventListener("click", function(event) {
+            event.preventDefault();
+            toggleButton(button);
+        });
+    });
+
+    //iterate text button
+    textFieldButtons.forEach(button => {
+        button.addEventListener("click", function(event) {
+            event.preventDefault();
+            toggleButton(button);
+        });
+    });
+
+    //buat ngaktivin state dari button  radio n text
+    function toggleButton(button) {
+        radioButtons.forEach(rb => rb.classList.remove("active"));
+        textFieldButtons.forEach(tb => tb.classList.remove("active"));
+        button.classList.add("active");
+    }
+});
---
+++ resources/views/AdminAddForm.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="/your-path-to-fontawesome/css/fontawesome.css" rel="stylesheet">
+    <link href="/your-path-to-fontawesome/css/brands.css" rel="stylesheet">
+    <link href="/your-path-to-fontawesome/css/solid.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link rel="stylesheet" href="/AdminAddForm.css">
+</head>
+
+<body>
+    <div id="formContainerWrapper">
+        <div class="form-template form-container" data-type="radio">
+            <div class="question-title">
+                <input type="text" class="option-text" placeholder="Enter Your question here">
+            </div>
+            <div class="question-card">
+                <div class="option-container">
+                    <input type="radio" class="option-input">
+                    <input type="text" class="option-text" placeholder="Enter option name" name="option[]">
+                    <button class="delete-question">remove</button>
+                </div>
+            </div>
+            <div class="Add-container">
+                <button type="button" class="button add-question-button">
+                    <span class="button__text">Add Question</span>
+                    <span class="button__icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" stroke="currentColor" height="24" fill="none" class="svg">
+                            <line y2="19" y1="5" x2="12" x1="12"></line>
+                            <line y2="12" y1="12" x2="19" x1="5"></line>
+                        </svg>
+                    </span>
+                </button>
+
+                <button class="Prototype">Add</button>
+            </div>
+            <div class="footer-question">
+                <label class="toggle">
+                    <input class="toggle-checkbox" type="checkbox" name="Required[]">
+                    <div class="toggle-switch"></div>
+                    <span class="toggle-label">Required</span>
+                </label>
+            </div>
+            <button class="remove-button">Remove</button>
+        </div>
+
+        <div class="form-template form-container-1" data-type="text">
+            <div class="question-title">
+                <input type="text" class="option-text" placeholder="Enter Your question here">
+            </div>
+            <div class="question-card">
+                <div class="option-container">
+                    <input type="text" class="option-text" placeholder="Enter option name" name="text[]">\
+                    <button class="delete-question">remove</button>
+                </div>
+            </div>
+            <div class="Add-container">
+                <button type="button" class="button add-question-button">
+                    <span class="button__text">Add Question</span>
+                    <span class="button__icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" stroke-width="2" stroke-linejoin="round" stroke-linecap="round" stroke="currentColor" height="24" fill="none" class="svg">
+                            <line y2="19" y1="5" x2="12" x1="12"></line>
+                            <line y2="12" y1="12" x2="19" x1="5"></line>
+                        </svg>
+                    </span>
+                </button>
+                <button class="Prototype">Add</button>
+            </div>
+            <label class="toggle">
+                <input class="toggle-checkbox" type="checkbox" name="Required[]">
+                <div class="toggle-switch"></div>
+                <span class="toggle-label">Required</span>
+            </label>
+            <button class="remove-button">Remove</button>
+        </div>
+    </div>
+
+    <div class="add-button">
+        <div class="button-container">
+            <div class="dropdown">
+                <button class="button-50" id="addNewButton" role="button">+ Add New</button>
+                <div class="dropdown-content">
+                    <button class="buttonss" id="radioButton"><a href="#" data-type="radio">Radio</a></button>
+                    <button class="buttonss" id="textFieldButton"><a href="#" data-type="text">Text Field</a></button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <script src="/AdminAddForm.js"></script>
+</body>
+
+</html>
