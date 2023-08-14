bef0025a6f84737294d517c049b6b74b708ec305
MuhammadFarrasDaffa <daffamfd27@gmail.com>
2023-08-14 17:02:05 +0700

    Tambah hasil form registration

---
+++ resources/views/ResultRegistrationForm.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+  <title>Registration Form</title>
+  <link rel="stylesheet" href="result.css" />
+  
+</head>
+
+<body>
+    <div class="column">
+        <section class="container">
+            <header>Thank You for Your Registration</header>
+            <p>Your registration has been completed. Here are the responses received.</p>
+        </section>
+
+        <section class="container">
+            <form action="#" class="form">
+            <div class="input-box">
+                <label>Fullname</label>
+                <input type="text" name="Fullname" placeholder="Name" disabled value="" />
+            </div>
+
+            <div class="input-box">
+                <label>Email Address</label>
+                <input type="text" name="Email" placeholder="Email" disabled value="" />
+            </div>
+
+            <div class="input-box">
+                <label>Organization</label>
+                <input type="text" name="Organization" placeholder="Organization" disabled value="" />
+            </div>
+
+            <div class="column">
+                <div class="input-box">
+                <label>Number Phone</label>
+                <input type="text" name="Phone" placeholder="Number Phone" disabled value="" />
+                </div>
+
+                <div class="input-box">
+                <label>Birthday</label>
+                <input type="date" name="Birthday" placeholder="dd-mm-yyyy" disabled value="" />
+                </div>
+            </div>
+
+            <div class="input-box">
+                <label>Gender</label>
+                <input type="text" name="Gender" placeholder="Gender" disabled value="" />
+            </div>
+
+
+            <div class="input-box">
+                <label>City Region</label>
+                <input type="text" name="City Region" placeholder="City Region" disabled value="" />
+            </div>
+
+            <div class="input-box">
+                <label>Complete Address</label>
+                <input type="text" name="City Region" placeholder="Address" disabled value="" />
+            </div>
+
+            <a href="/homepage"><button type="button" >Homepage</button></a>
+            </div>
+            </form>
+        </section>
+    </div>
+</body>
+
+</html>
