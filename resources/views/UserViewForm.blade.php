0175a1dc360ed499fb4121c63812b589b55824f3
fahrigunawan <fahrigilo991@gmail.com>
2023-08-23 06:21:51 +0700

    fix form

--- resources/views/UserViewForm.blade.php
+++ resources/views/UserViewForm.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Form Maker</title>
     <link rel="stylesheet" href="/UserViewForm.css" />
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous" />
   </head>
   <body>
@@ -30,13 +31,6 @@
                 </div>
 
                 <div class="form-container">
-                    <div class="input-group">
-                        <label class="label">Email address</label>
-                        <input autocomplete="off" name="Email" id="Email" class="input" type="email">
-                    </div>
-                </div>
-
-                <div class="form-container">
                     <label class="label">Email address</label>
                     <h4 style="color: red;">*</h4>
                     <div class="radio-wrap">
@@ -49,6 +43,24 @@
                             <input id="03" type="radio" name="r" value="3">
                         </form>
                     </div>
+                </div>
+
+                <div class="form-container">
+                    <div class="-mx-4 flex flex-wrap">
+                        <div class="w-full px-4 md:w-1/2 lg:w-1/3">
+                           <div class="mb-12">
+                              <label class="mb-3 block text-base font-medium text-black">
+                              Name
+                              </label>
+                              <h4 style="color: red;">*</h4>
+                              <div class="relative">
+                                 <input type="text" placeholder="Devid Jhon" class="w-full rounded-md border border-form-stroke p-3 pl-12 text-black placeholder-[#929DA7] outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-[#F5F7FD]">
+                                 <span class="absolute top-1/2 left-4 -translate-y-1/2">
+                                 </span>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
                 </div>
             </form>
         </div>
