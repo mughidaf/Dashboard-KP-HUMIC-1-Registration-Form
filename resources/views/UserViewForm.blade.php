8a9e44f3b55e7a023744e2069f3375490648ef0a
mughidaf <ghiyats02@gmail.com>
2023-08-23 09:39:47 +0700

    Nyicil backend userView

--- resources/views/UserViewForm.blade.php
+++ resources/views/UserViewForm.blade.php
@@ -10,77 +10,79 @@
   </head>
   <body>
     <header>
-      <h1>Form Maker</h1>
+      <h1>Registration Form</h1>
     </header>
     <main>
         <div class="container">
+            <div class="form-container">
+                    <img src="/image/Humic-Logi.png" alt="Banner" >
+                    <div class="main">
+                    <h1>{{$form->judul}}</h1>
+                    <hr />
+                    <p>
+                        {{$form->deskripsi}}
+                    </p>
+                    </div>
+            </div>
             <form id="formCreator" action="storeForm" method="post">
             @csrf
-                <div class="form-container">
-                        <img src="/image/Humic-Logi.png" alt="Banner" >
-                        <div class="main">
-                        <h1>The Question</h1>
-                        <hr />
-                        <p>
-                            The deskribsi
-                        </p>
-                        <h4 style="color: red;">* Required</h4>
-                        </div>
-                </div>
-
-                <div class="form-container">
-                    <label class="label">Email address</label>
-                    <h4 style="color: red;">*</h4>
-                    <div class="radio">
-                        <input id="radio-1" name="radio" type="radio" checked>
-                        <label for="radio-1" class="radio-label">ch1</label>
-                      </div>
-                    
-                      <div class="radio">
-                        <input id="radio-2" name="radio" type="radio">
-                        <label  for="radio-2" class="radio-label">ch2</label>
+                @foreach ($form->Questions as $tanya)
+                    @if ($tanya->type == 'teks')
+                    <div class="form-container">
+                        <div class="-mx-4 flex flex-wrap">
+                            <div class="w-full">
+                               <div class="mb-12">
+                                  <label class="mb-3 block text-base font-medium text-black">
+                                  {{$tanya->question}}
+                                  </label>
+                                  <h4 style="color: red;">*</h4>
+                                  <div class="relative">
+                                     <input type="text" placeholder={{$tanya->question}} class="w-full rounded-md border border-form-stroke p-3 pl-12 text-black placeholder-[#929DA7] outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-[#F5F7FD]">
+                                     <span class="absolute top-1/2 left-4 -translate-y-1/2">
+                                     </span>
+                                  </div>
+                               </div>
+                            </div>
+                         </div>
                     </div>
-                </div>
+                    @endif
 
-                <div class="form-container">
-                    <div class="-mx-4 flex flex-wrap">
-                        <div class="w-full">
-                           <div class="mb-12">
-                              <label class="mb-3 block text-base font-medium text-black">
-                              Name
-                              </label>
-                              <h4 style="color: red;">*</h4>
-                              <div class="relative">
-                                 <input type="text" placeholder="Devid Jhon" class="w-full rounded-md border border-form-stroke p-3 pl-12 text-black placeholder-[#929DA7] outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-[#F5F7FD]">
-                                 <span class="absolute top-1/2 left-4 -translate-y-1/2">
-                                 </span>
-                              </div>
-                           </div>
+                    @if ($tanya->type == 'file')
+                        {{-- file upload --}}
+                    <div class="form-container">
+                        <label class="mb-3 block text-base font-medium text-black">
+                            {{$tanya->question}}
+                        </label>
+                        <h4 style="color: red;">*</h4>
+                        <div class="upload-container">
+                            <div class="upload-icon">
+                                <i class="fas fa-cloud-upload-alt"></i>
+                            </div>
+                            <div class="upload-text">
+                                <p>Drag and drop your files here or</p>
+                                <label for="file-input" class="custom-btn">Browse</label>
+                                {{-- accep tipe file nya --}}
+                                <input type="file" id="file-input" class="file-input" accept=".png,.jpg,.jpeg">
+                            </div>
                         </div>
-                     </div>
-                </div>
+                    </div>
+                    @endif
 
+                    @if ($tanya->type == 'choice')
+                    <div class="form-container">
+                        <label class="label">{{$tanya->question}}</label>
+                        <h4 style="color: red;">*</h4>
+                        @foreach ($tanya->Options as $opsi)
+                        <div class="radio">
+                            <input id="radio-1" name="radio" type="radio" checked>
+                            <label for="radio-1" class="radio-label">{{$opsi->option}}</label>
+                          </div>
+                        @endforeach
+                    </div>
+                    @endif
+                @endforeach
                 <div class="form-container">
                     {{-- ttd --}}
-                </div>
-
-                {{-- file upload --}}
-                <div class="form-container">
-                    <label class="mb-3 block text-base font-medium text-black">
-                        Name
-                    </label>
-                    <h4 style="color: red;">*</h4>
-                    <div class="upload-container">
-                        <div class="upload-icon">
-                            <i class="fas fa-cloud-upload-alt"></i>
-                        </div>
-                        <div class="upload-text">
-                            <p>Drag and drop your files here or</p>
-                            <label for="file-input" class="custom-btn">Browse</label>
-                            {{-- accep tipe file nya --}}
-                            <input type="file" id="file-input" class="file-input" accept=".png,.jpg,.jpeg">
-                        </div>
-                    </div>
                 </div>
             </form>
         </div>
