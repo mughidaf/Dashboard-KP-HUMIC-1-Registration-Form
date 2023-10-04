07d43d6192ad95585aa67e90d0471fa181ce28a4
fahrigunawan <fahrigilo991@gmail.com>
2023-10-05 05:07:21 +0700

    new table

--- public/MoreOption.js
+++ public/MoreOption.js
@@ -1,29 +1,69 @@
-//export berdasarkan data type
-var $table = $('#table');
+// //export berdasarkan data type
+// var $table = $('#table');
+// $(function() {
+//     $('#toolbar').find('select').change(function() {
+//         $table.bootstrapTable('refreshOptions', {
+//             exportDataType: $(this).val()
+//         });
+//     });
+// })
 
-$(function() {
-    $('#toolbar').find('select').change(function() {
-        $table.bootstrapTable('refreshOptions', {
-            exportDataType: $(this).val()
-        });
+// var trBoldBlue = $("table");
+
+// $(trBoldBlue).on("click", "tr", function() {
+//     $(this).toggleClass("bold-blue");
+// });
+
+// //total data
+
+// function idFormatter(data) {
+//     hasil = data.length
+//     return 'Total Data : ' + hasil;
+// }
+
+// //Buat ngecek value responses
+// function updateResponsesCount() {
+//     var h1Element = document.getElementById("responsesCount");
+//     h1Element.textContent = "Number of responses : " + hasil;
+// }
+
+
+
+
+$(document).ready(function() {
+    $('#example').DataTable({
+        dom: 'Bfrtip',
+        buttons: [{
+            extend: 'collection',
+            className: 'custom-html-collection',
+            stateSave: true,
+            buttons: [
+                '<h3>Sort</h3>',
+                'columnsToggle',
+                '<h3>Export</h3>',
+                // 'pdf',
+                'csv',
+                'excel',
+
+                '<h3>PDF Export</h3>',
+                {
+                    extend: 'print',
+                    customize: function(win) {
+                        $(win.document.body)
+                            .css('font-size', '10pt')
+                            .prepend(
+                                '<img src="" style="position:absolute; top:0; left:0;" />'
+                            );
+                        $(win.document.body).find('table')
+                            .addClass('compact')
+                            .css('font-size', 'inherit');
+                    },
+                    text: 'PDF'
+                },
+            ],
+            select: true,
+            text: 'Sort & Export',
+        }, ],
+        select: true
     });
-})
-
-var trBoldBlue = $("table");
-
-$(trBoldBlue).on("click", "tr", "th", function() {
-    $(this).toggleClass("bold-blue");
 });
-
-//total data
-
-function idFormatter(data) {
-    hasil = data.length
-    return 'Total Data : ' + hasil;
-}
-
-//Buat ngecek value responses
-function updateResponsesCount() {
-    var h1Element = document.getElementById("responsesCount");
-    h1Element.textContent = "Number of responses : " + hasil;
-}
--- resources/views/MoreOption.blade.php
+++ resources/views/MoreOption.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -18,8 +18,9 @@
         <!--ini buat value apa yang mau di export-->
         <div id="toolbar">
                 <select class="form-control">
-                        <option value="all">Export All</option>
-                        <option value="selected">Export Selected</option>
+                    <option value="">Export Basic</option>
+                    <option value="all">Export All</option>
+                    <option value="selected">Export Selected</option>
                 </select>
         </div>
 
@@ -28,20 +29,17 @@
             <h1 id="responsesCount">Number of responses : <span>{{count($question->Answers)}}</span></h1>
         </div>
         
-        {{-- <table id="table"   
-                    data-toggle="table"
-                    data-height="460"
-                    data-search="true"
-                    data-filter-control="true" 
-                    data-show-export="true"
-                    data-show-footer="true"
-                    data-pagination="true"
-                    data-total-field="count"
-                    data-toolbar="#toolbar"
-                    data-show-refresh="true"
-                    data-data-field="items">
+        
+
+        <table id="table" 
+            data-toggle="table"
+            data-search="true"
+            data-filter-control="true" 
+            data-show-export="true"
+            data-click-to-select="true"
+            data-toolbar="#toolbar"
+            class="table-responsive">
             <thead>
-                <!--data-filter-control="". itu buat tipe buat nyari data yang sama, ada input sama select -->
                 <tr>
                     <th data-field="state" data-checkbox="true"></th>
                     <th data-field="ID" data-footer-formatter="idFormatter" data-filter-control="select" data-sortable="true">Submission ID</th>
@@ -58,53 +56,11 @@
                             $nama = $identitas->Answers[$i]->answer;
                         }
                     @endphp
+
                     <tr>
                         <td class="bs-checkbox "><input data-index="1" name="btSelectItem" type="checkbox"></td>
-                        <td>{{$respon->subID}}</td>
-                        @if ($identitas != null)
-                            <td>{{$nama}}</td>
-                        @else
-                            <td>-</td>
-                        @endif
-
-                        @if ($question->type == "file")
-                        <a href="youtube.com">
-                            <td>
-                               Test 
-                            </td>
-                        </a>
-                        @elseif ($question->type == "ttd")
-                            <td>
-                                <img src="storage/{{$respon->answer}}" alt="">
-                            </td>
-                        @else   
-                            <td>{{$respon->answer}}</td>
-                        @endif
-                    </tr>
-                @endfor
-            </tbody>
-        </table> --}}
-
-        <table>
-            <thead>
-                <tr>
-                    <th>Sub ID</th>
-                    <th>Name</th>
-                    <th>Respon</th>
-                </tr>
-            </thead>
-            <tbody>
-                <!--1 tr satu kolum-->
-                @for ($i = 0; $i < count($question->Answers); $i++)
-                    @php
-                        $respon = $question->Answers[$i];
-                        if ($identitas != null) {
-                            $nama = $identitas->Answers[$i]->answer;
-                        }
-                    @endphp
-                    <tr>
-                        <td class="bs-checkbox "><input data-index="1" name="btSelectItem" type="checkbox"></td>
-                        <td>{{$respon->subID}}</td>
+                        <td >{{$respon->subID}}</td>
+                        
                         @if ($identitas != null)
                             <td>{{$nama}}</td>
                         @else
@@ -113,7 +69,7 @@
 
                         @if ($question->type == "file")
                             <td>
-                               <a href="youtube.com">dsada</a>
+                                <a href="http://google.com">{{$respon->answer}}</a>
                             </td>
                         @elseif ($question->type == "ttd")
                             <td>
@@ -129,7 +85,7 @@
         </table>
     </div>
 
-
+    <script src="extensions/defer-url/bootstrap-table-defer-url.js"></script>
     <script src="https://unpkg.com/bootstrap-table@1.22.1/dist/bootstrap-table.min.js"></script>
     <script src="/MoreOption.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
@@ -139,5 +95,93 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.9.1/extensions/export/bootstrap-table-export.js"></script>
     <script src="//rawgit.com/hhurz/tableExport.jquery.plugin/master/tableExport.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.9.1/extensions/filter-control/bootstrap-table-filter-control.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/tableexport.jquery.plugin@1.10.21/tableExport.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/tableexport.jquery.plugin@1.10.21/libs/jsPDF/jspdf.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/tableexport.jquery.plugin@1.10.21/libs/jsPDF-AutoTable/jspdf.plugin.autotable.js"></script>
+    
+</body>
+</html> --}}
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/MoreOption.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>{{$question->question}}</h1>
+    <table id="example" class="display table table-bordered table-hover nowrap" cellspacing="0" width="100%">
+        <thead>
+            <tr> 
+                <th>ID</th>
+                <th>Name</th>
+                <th>Response</th>
+
+            </tr>
+        </thead>
+        <tfoot>
+            <tr>
+                <th>
+                    <p id="responsesCount">Number of responses : <span>{{count($question->Answers)}}</span></p>
+                </th>
+                <th></th>
+                <th></th>
+                
+            </tr>
+        </tfoot>
+            <tbody>
+                <!--1 tr satu kolum-->
+                @for ($i = 0; $i < count($question->Answers); $i++)
+                    @php
+                        $respon = $question->Answers[$i];
+                        if ($identitas != null) {
+                            $nama = $identitas->Answers[$i]->answer;
+                        }
+                    @endphp
+                    <tr>
+                        <td >{{$respon->subID}}</td>
+                        
+                        @if ($identitas != null)
+                            <td>{{$nama}}</td>
+                        @else
+                            <td>-</td>
+                        @endif
+
+                        @if ($question->type == "file")
+                            <td>
+                                <a href="google.com">{{$respon->answer}}</a>
+                            </td>
+                        @elseif ($question->type == "ttd")
+                            <td>
+                                <img src="{{$respon->answer}}" alt="sadsa">
+                            </td>
+                        @else   
+                            <td>{{$respon->answer}}</td>
+                        @endif
+                    </tr>
+                @endfor
+            </tbody>
+    </table>
+    
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="/MoreOption.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.colVis.min.js"></script>
+    <script src="https://cdn.datatables.net/select/1.7.0/js/dataTables.select.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+    
+    
 </body>
 </html>
